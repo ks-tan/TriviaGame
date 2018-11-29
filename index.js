b2cafa79ab7e921514b803dd4eb4b9208458e793
Tan Kang Soon <tankangsoon@gmail.com>
2018-11-29 21:50:44 +0800

    Allow new players to enter new game, then reflect current question

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
@@ -6,23 +7,38 @@ const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
 
+let clientsData = [];
+let currentQuestionNumber = 0;
+const triviaData = [
+  { q: "What is KS's favourite food?", a: "French Fries" }, 
+  { q: "What is Lois's new favourite game?", a: "Smash Bros" }
+];
+
 server.listen(5000, function () {
   console.log("Server started on port 5000");
 })
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/admin", function(req, res) {
+  res.sendFile(__dirname + "/admin.html");
+});
+
 io.on("connection", function(socket) {
-  const serverMessage = {message: "PING"}
-  let count = 11;
-  socket.emit("server-ping", serverMessage)
-  socket.on("client-pong", (data) => {
-    console.log(data.message)
-    if (count > 0) {
-      socket.emit("server-ping", serverMessage)
-      count --
+  const serverMessage = {message: "Welcome to Trivia Game!"};
+  socket.emit("onConnection", serverMessage);
+
+  socket.on("disconnect", () => {
+    _.remove(clientsData, (data) => {return data.socketId === socket.id;});
+  });
+
+  socket.on("onClientRegister", (data) => {
+    //Only update `clientsData` if the client's `sockedId` is already recorded
+    if (_.find(clientsData, (client) => {return client.socketId === socket.id}) === undefined) { 
+      clientsData.push({socketId: socket.id, name: data.clientName});
+      socket.emit("onNewQuestion", {triviaObject: triviaData[currentQuestionNumber]});
     }
   })
 });
