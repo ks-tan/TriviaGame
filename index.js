fa4cfa58b4aeef9118cbdfb85545de48bfb7a4d0
Tan Kang Soon <tankangsoon@gmail.com>
2018-11-30 21:44:53 +0800

    Finish display.css

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ server.listen(process.env.PORT || 5000, function () {
   console.log("Server started on port 5000");
 })
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
